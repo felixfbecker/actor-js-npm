448e48f678b53fe006ba0b77cd2a56c9d81136a1
Dave Gaeddert <dave.gaeddert@gmail.com>
2017-06-12 22:47:38 -0500

    only do pr for highest version, fix yarn.lock tests

--- entrypoint.js
+++ entrypoint.js
@@ -31,63 +31,60 @@ dependencies.forEach(function(dependency) {
   const packageJson = require(packageJsonPath)
   const isDevDependency = packageJson.hasOwnProperty('devDependencies') && packageJson.devDependencies.hasOwnProperty(name)
 
-  dependency.available.forEach(function(available) {
+  const version = dependency.available[0].version
+  const branchName = `${name}-${version}-#${BUILD_NUMBER}`
+  const msg = `Update ${name} from ${installed} to ${version}`
+  const prBody = `${name} has been updated to ${version} by dependencies.io`
 
-    const version = available.version
-    const branchName = `${name}-${version}-#${BUILD_NUMBER}`
-    const msg = `Update ${name} from ${installed} to ${version}`
-    const prBody = `${name} has been updated to ${version} by dependencies.io`
+  shell.rm('-rf', 'node_modules')
 
-    shell.rm('-rf', 'node_modules')
+  shell.exec(`git checkout ${GIT_SHA}`)
 
-    shell.exec(`git checkout ${GIT_SHA}`)
+  shell.exec(`git checkout -b ${branchName}`)
 
-    shell.exec(`git checkout -b ${branchName}`)
+  if (hasYarnLockFile) {
+    shell.exec(`yarn upgrade ${name}@${version}`)
+    shell.exec(`git add ${packageJsonPath} ${yarnLockPath}`)
+  }
+  else if (hasPackageLockFile) {
+    shell.exec('npm install --quiet')
+    shell.exec(`npm install ${name}@${version} --quiet`)
+    shell.exec(`git add ${packageJsonPath} ${packageLockJsonPath}`)
+  } else {
+    const installOpts = isDevDependency ? '--save-dev' : ''
+    shell.exec('npm install --quiet')
+    shell.exec(`npm install ${name}@${version} --quiet --save --save-exact ${installOpts}`)
+    shell.exec(`rm ${packageLockJsonPath}`)
+    shell.exec(`git add ${packageJsonPath}`)
+  }
 
-    if (hasYarnLockFile) {
-      shell.exec(`yarn upgrade ${name}@${version}`)
-      shell.exec(`git add ${packageJsonPath} ${yarnLockPath}`)
-    }
-    else if (hasPackageLockFile) {
-      shell.exec('npm install --quiet')
-      shell.exec(`npm install ${name}@${version} --quiet`)
-      shell.exec(`git add ${packageJsonPath} ${packageLockJsonPath}`)
-    } else {
-      const installOpts = isDevDependency ? '--save-dev' : ''
-      shell.exec('npm install --quiet')
-      shell.exec(`npm install ${name}@${version} --quiet --save --save-exact ${installOpts}`)
-      shell.exec(`rm ${packageLockJsonPath}`)
-      shell.exec(`git add ${packageJsonPath}`)
-    }
+  shell.exec(`git commit -m "${msg}"`)
 
-    shell.exec(`git commit -m "${msg}"`)
+  // fail if there are other unchanged files
+  if (shell.exec('git status --porcelain').stdout.trim() != "") {
+      throw 'Git repo is dirty, there are changes that aren\'t accounted for\n' + shell.exec('git status').stdout
+  }
 
-    // fail if there are other unchanged files
-    if (shell.exec('git status --porcelain').stdout.trim() != "") {
-        throw 'Git repo is dirty, there are changes that aren\'t accounted for\n' + shell.exec('git status').stdout
-    }
-
-    if (!TESTING) {
-      shell.exec(`git push --set-upstream origin ${branchName}`)
-      const requestOptions = {
-        method: 'POST',
-        json: {
-          'title': msg,
-          'head': branchName,
-          'base': PR_BASE,
-          'body': prBody,
-        },
-        url: `https://api.github.com/repos/${GITHUB_REPO_FULL_NAME}/pulls`,
-        headers: {
-          'User-Agent': 'dependencies.io actor-js-npm',
-          'Authorization': `token ${GITHUB_API_TOKEN}`
-        }
+  if (!TESTING) {
+    shell.exec(`git push --set-upstream origin ${branchName}`)
+    const requestOptions = {
+      method: 'POST',
+      json: {
+        'title': msg,
+        'head': branchName,
+        'base': PR_BASE,
+        'body': prBody,
+      },
+      url: `https://api.github.com/repos/${GITHUB_REPO_FULL_NAME}/pulls`,
+      headers: {
+        'User-Agent': 'dependencies.io actor-js-npm',
+        'Authorization': `token ${GITHUB_API_TOKEN}`
       }
-      request(requestOptions).on('response', function(response) {
-        // console.log(response)
-      })
     }
-  })
+    request(requestOptions).on('response', function(response) {
+      // console.log(response)
+    })
+  }
 
   dependencyJSON = JSON.stringify(dependency)
   console.log(`BEGIN_DEPENDENCIES_SCHEMA_OUTPUT>${dependencyJSON}<END_DEPENDENCIES_SCHEMA_OUTPUT`)
